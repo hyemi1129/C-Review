8e0d939bb64654cc2f950d94728ef3eb4aabccf9
hyemi1129 <kimhyemi1243@gmail.com>
2024-02-08 12:49:37 +0900

    Review of loops3

--- for.c
+++ for.c
@@ -66,7 +66,7 @@ int main(){
 	}
 } */
 
-#include <stdio.h>
+/* #include <stdio.h>
 
 int main(){
 	int n, i, sum=0;
@@ -77,4 +77,127 @@ int main(){
 		}
 	}
 	printf("%d", sum);
-}
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int n, i, sum=0;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		sum+=i;
+		
+		if(sum>=n){
+			printf("%d", i);
+			break;
+		}
+	}
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int n, i;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		if(i%3==0){
+			printf("X ");
+	}
+	else{
+		printf("%d ", i);
+	}
+	}
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int n, i, sum=0;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		sum += i;
+		if(sum>=n){
+			printf("%d", sum);
+			break;
+		}
+	}
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int n, i;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		if(i%3==0){
+			printf("");
+		}
+		else{
+			printf("%d ", i);
+		}
+		
+	}
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int a, d, n, i;
+	scanf("%d %d %d", &a,&d,&n);
+	for(i=1; i<n; i++){
+		a = a+d;
+	}
+	printf("%d\n", a);
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int a, b, i, temp;
+	scanf("%d %d", &a, &b);
+	if(a>b){
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	for(i=a; i<=b; i++){
+		printf("%d ", i);
+	}
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	char a, b, i;
+	scanf("%c %c", &a, &b);
+	for(i=a; i<=b; i++)
+	printf("%c ", i);
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int n, m, i, j;
+	scanf("%d %d", &n, &m);
+	for(i=1; i<=n; i++){
+		for(j=1; j<=m; j++){
+			printf("%d %d\n", i, j);
+		}
+	}
+} */
+
+/* #include <stdio.h>
+
+int main(){
+	int r, g, b, i, j, k;
+	scanf("%d %d %d", &r, &g, &b);
+	for(i=0; i<r; i++){
+		for(j=0; j<g; j++){
+			for(k=0; k<b; k++){
+				printf("%d %d %d\n", i, j, k);
+			}
+		}
+	}
+	printf("%d", r*g*b);
+} */
+
